a6135c6e4f0297f4cc1e311d44255e1b1d70f719
khubaib shah <bewithbaacha@gmail.com>
2025-01-25 02:45:13 +0500

    fixed importing btn

--- src/pages/AddBundle.jsx
+++ src/pages/AddBundle.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { toast } from "sonner";
 import { createItem } from "@/services/apiService";
+import { Button } from "@/components/ui/Button";
 
 const AddBundle = ({ onAddBundle }) => {
   const [formData, setFormData] = useState({
